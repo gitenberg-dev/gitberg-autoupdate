54948ebcc4002766309447bace8d04cabd2b4cd1
eric <eric@hellman.net>
2021-09-14 16:27:00 -0400

    xvbf is superfluous

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(name='gitberg.autoupdate',
         'sh>=1',
       ],
       install_requires=[
-          'gitberg>=0.7.5',
+          'gitberg>=0.7.6',
           'boto3',
           'pyepub>=0.5.0',
       ],
